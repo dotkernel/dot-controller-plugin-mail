b60acd2d87e52db440632ede984909e43e1c5480
n3vrax <n3vrax@gmail.com>
2017-03-11 00:51:24 +0200

    Updated php file headers doc blocks

---
+++ CHANGELOG.md
@@ -0,0 +1,30 @@
+## 0.1.1 - 2017-03-11
+
+### Added
+* Updated php file headers doc blocks
+
+### Deprecated
+* Nothing
+
+### Removed
+* Nothing
+
+### Fixed
+* Nothing
+
+
+## 0.1.0 - 2017-03-09
+
+Initial tagged release
+
+### Added
+* Everything
+
+### Deprecated
+* Nothing
+
+### Removed
+* Nothing
+
+### Fixed
+* Nothing
--- src/Factory/MailPluginAbstractFactory.php
+++ src/Factory/MailPluginAbstractFactory.php
@@ -1,10 +1,8 @@
 <?php
 /**
- * @copyright: DotKernel
- * @library: dotkernel/dot-controller-plugin-mail
- * @author: n3vrax
- * Date: 9/6/2016
- * Time: 7:49 PM
+ * @see https://github.com/dotkernel/dot-controller-plugin-mail/ for the canonical source repository
+ * @copyright Copyright (c) 2017 Apidemia (https://www.apidemia.com)
+ * @license https://github.com/dotkernel/dot-controller-plugin-mail/blob/master/LICENSE.md MIT License
  */
 
 namespace Dot\Controller\Plugin\Mail\Factory;
